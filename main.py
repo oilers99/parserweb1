c822f9ac77f72a22d39fe168f58d0aaa9e8a0eb3
oilers <gdss@yandex.ru>
2023-03-20 16:50:25 +0300

    start

---
+++ main.py
@@ -0,0 +1,19 @@
+def parser(url):
+    """Принимает URL поиск-запроса. Из VISUAL BUTTON_CLICK
+    Возврашает Список значений [цена, цена за м2]
+    Передает в MAIN CONVERT"""
+    pass
+
+
+def convert(parser_result):
+    """Принимет необработанный список цен из PARSER
+    Убирает все лишнее, переводиn в INT
+    Передает в MAIN AVERAGE_PRICE"""
+    pass
+
+
+def average_price(convert_result):
+    """Принмает список из MAIN CONVERT
+    высчитывает среднию
+    Передает в VISUAL RESULT_PRICE"""
+    pass
---
+++ visual.py
@@ -0,0 +1,15 @@
+def windows():
+    """Создает Окно с одним полем ввода, кнокой"""
+    pass
+
+
+def button_click():
+    """Обработка нажатия кнопки
+    Передаёт в MAIN PARSER url поиск-запрса"""
+    pass
+
+
+def result_price():
+    """Принимает значение из MAIN AVERAGE_PRICE
+    Выводит в Label"""
+    pass
