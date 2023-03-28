1fe127225b644446d74ba3427ac6ce49c4b9e852
oilers <gdss@yandex.ru>
2023-03-28 09:12:07 +0300

    finish

--- main.py
+++ main.py
@@ -1,19 +1,94 @@
+import visual
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
+
+
 def parser(url):
     """Принимает URL поиск-запроса. Из VISUAL BUTTON_CLICK
     Возврашает Список значений [цена, цена за м2]
     Передает в MAIN CONVERT"""
-    pass
+
+    """запуск вэб драйвера и ананимность"""
+    options = webdriver.ChromeOptions()
+    headers = "User-Agent=" + """Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36
+     (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"""
+    options.add_argument(headers)
+    # фоновый режим
+    options.add_argument("--headless")
+
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    service = Service(executable_path="C:\\Users\\oilers\\PycharmProjects\\parserweb\\chrome_driver\\chromedriver.exe", )
+    driver = webdriver.Chrome(service=service, options=options)
+
+
+    """Перебор по странице"""
+    list_price = []
+    try:
+        driver.get(url=url)
+        time.sleep(5)
+
+        for element in driver.find_elements(By.CLASS_NAME, "iva-item-priceStep-uq2CQ"):
+            time.sleep(3)
+            temp = element.text
+            list_price.append(temp)
+            # print(list_price)
+            time.sleep(4)
+
+    except Exception as ex:
+        print(ex)
+
+    finally:
+        driver.close()
+        driver.quit()
+
+    convert(parser_result=list_price)
 
 
 def convert(parser_result):
     """Принимет необработанный список цен из PARSER
     Убирает все лишнее, переводиn в INT
     Передает в MAIN AVERAGE_PRICE"""
-    pass
+
+    parser_result = [x.split('\n')[1] for x in parser_result]
+    parser_result = [x.split(' ')[0:2] for x in parser_result]
+
+    list_result = []
+    for i in parser_result:
+        parser_result = "".join(i)
+        list_result.append(parser_result)
+
+    list_result = [int(x) for x in list_result]
+
+    average_price(convert_result=list_result)
 
 
 def average_price(convert_result):
     """Принмает список из MAIN CONVERT
     высчитывает среднию
     Передает в VISUAL RESULT_PRICE"""
-    pass
+
+    """подсчет средней цены"""
+    ave_price = sum(convert_result) // len(convert_result)
+
+    """подсчет средневзвешенной цены"""
+    hi_stop = (sum(convert_result) // len(convert_result)) * 1.2
+    lo_stop = (sum(convert_result)) // len(convert_result) * 0.8
+    list_fair_price = []
+    for i in convert_result:
+        if i < hi_stop and i > lo_stop:
+            list_fair_price.append(i)
+
+    fair_price = sum(list_fair_price) // len(list_fair_price)
+
+    visual.result_price(average=ave_price, fair=fair_price)
+
+
+def main():
+    """1"""
+    visual.windows()
+
+
+if __name__ == "__main__":
+    main()
--- visual.py
+++ visual.py
@@ -1,15 +1,50 @@
+import tkinter
+from tkinter import *
+import main
+
+
 def windows():
     """Создает Окно с одним полем ввода, кнокой"""
-    pass
+    def button_click1():
+        """Обработка нажатия кнопки
+        Передаёт в MAIN PARSER url поиск-запрса"""
+        url_click = url_entry.get()
+        main.parser(url=url_click)
+
+    """основное окно"""
+    top = tkinter.Tk()
+    top.resizable(False, False)
+    top.title("AvitoParser")
+    top.geometry("720x150")
+
+    """Элементы при старте"""
+    # Текст
+    lbl1 = Label(top, text="Вставь ссылку", font=20)
+    lbl1.grid(row=0, column=0)
+    top.grid_columnconfigure(0, minsize=200)
+
+    # Поле ввода
+    url_entry = tkinter.Entry(top)
+    url_entry.grid(row=0, column=1, sticky=EW, pady=10)
+    top.grid_columnconfigure(1, minsize=400)
+
+    # Кнопка
+    btn_start = tkinter.Button(top, text="Start", command=button_click1)
+    btn_start.grid(row=0, column=2, padx=5, sticky=EW)
+    top.grid_columnconfigure(2, minsize=100)
+
+    top.mainloop()
 
 
-def button_click():
-    """Обработка нажатия кнопки
-    Передаёт в MAIN PARSER url поиск-запрса"""
-    pass
 
-
-def result_price():
+def result_price(average, fair):
     """Принимает значение из MAIN AVERAGE_PRICE
     Выводит в Label"""
-    pass
+
+    average_lbl = Label(text=f"{average} Средняя цена за м2 (весь рынок)", font=18)
+    average_lbl.grid(row=1, column=1)
+
+    fair_lbl = Label(text=f"{fair} Средневзвешенная цена за м2", font=18)
+    fair_lbl.grid(row=2, column=1)
+
+
